3ee342da75a5983394af25918f8f03268fce91bd
Liu Wenyuan <15816141883@163.com>
2023-07-12 11:46:52 +0800

    Overhaul boot and add new HAS screen

--- mod.lua
+++ mod.lua
@@ -1,8 +1,29 @@
+---@alias WiiStates
+---| "HealthAndSafety" # The "warning - health and safety" screen seen when you first enter System Menu
+
+---@type table<WiiStates, table>
+Mod.States = {}
+
 function Mod:init()
-    print("Loaded "..self.info.name.."!")
+    Mod.States = {
+        ["HealthAndSafety"] = HealthAndSafetyScreen
+    }
+
+    ---@diagnostic disable-next-line: redundant-return-value
+    Utils.hook(Game, "save", function() return {} end)
 end
 
-function Mod:load()
-    self.warning_screen = WarningScreen()
-    Game.stage:addChild(self.warning_screen)
+function Mod:postInit()
+    self:setState("HealthAndSafety")
+end
+
+--- Switches the Gamestate to the given one.
+---@param state table|WiiStates The gamestate to switch to.
+---@param ... any Arguments passed to the gamestate.
+function Mod:setState(state, ...)
+    if type(state) == "string" then
+        Gamestate.switch(Mod.States[state] or Kristal.States[state], ...)
+    else
+        Gamestate.switch(state, ...)
+    end
 end
