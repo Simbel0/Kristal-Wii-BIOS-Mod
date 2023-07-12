170fd886a18a3cc1021a8570559976843b6bd739
Liu Wenyuan <15816141883@163.com>
2023-07-13 06:12:59 +0800

    Move Mod:isAmerican to mod.lua
    We're not splitting mod.lua yet

--- mod.lua
+++ mod.lua
@@ -1,10 +1,11 @@
+-- Various mod-specific game states.
+-- (it's probably a good idea to not split everything into their
+-- own states)
 ---@alias WiiStates
----| "HealthAndSafety" # The "warning - health and safety" screen seen when you first enter System Menu
+---| "HealthAndSafety" # The "warning - health and safety" screen that is seen on boot
 
 ---@type table<WiiStates, table>
 Mod.States = {}
-
-modRequire("_luals_annonations/getComputerRegion")
 
 function Mod:init()
     Mod.States = {
@@ -29,4 +30,21 @@ function Mod:setState(state, ...)
     else
         Gamestate.switch(state, ...)
     end
+end
+
+-- Whether to use US-ENG-style HAS screen or not.
+-- For some reason, USA Wii consoles are the only ones where the warning screen
+-- was all white instead of colored
+function Mod:isAmerican()
+    local locale
+    if love.system.getOS() == "Windows" then
+        -- On MS-Win LOCALE is probably not set normally
+        locale = os.setlocale("")
+        local start = locale:find("_")
+        local end_str = locale:find("%.", start+1)
+        return locale:sub(start+1, end_str-1) == "United States"
+    end
+
+    locale = os.getenv("LC_ALL") or os.getenv("LANG")
+    return locale:match("%a%a.(%a%a)") == "US"
 end
