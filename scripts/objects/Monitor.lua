9c3e8cd6ba619b3c69436e058aa0e7ec99064265
Simbel0 <61120464+Simbel0@users.noreply.github.com>
2023-07-20 20:38:10 +0200

    dumb moment 2: electric electrocution

--- scripts/objects/Monitor.lua
+++ scripts/objects/Monitor.lua
@@ -34,10 +34,10 @@ function Monitor:draw(alpha)
 	love.graphics.setShader(Mod.Shaders["RemoveColor"])
 
 	love.graphics.setColor(155/255, 155/255, 155/255, alpha)
-	love.graphics.draw(self.edge, self.x, self.y, 0, 1.15, 1.1, 0.5, 0.5)
-	love.graphics.draw(self.edge, self.x+self.edge:getWidth()*2.15, self.y, math.rad(90), 1.15, 1.1, 0.5, 0.5)
-	love.graphics.draw(self.edge, self.x+self.edge:getWidth()*2.15, self.y+self.edge:getHeight()*1.65, math.rad(180), 1.15, 1.1, 0.5, 0.5)
-	love.graphics.draw(self.edge, self.x, self.y+self.edge:getHeight()*1.65, math.rad(270), 1.15, 1.1, 0.5, 0.5)
+	love.graphics.draw(self.edge, 50, 15, 0, 1.15, 1.1, 0.5, 0.5)
+	love.graphics.draw(self.edge, 50+self.edge:getWidth()*2.15, 15, math.rad(90), 1.15, 1.1, 0.5, 0.5)
+	love.graphics.draw(self.edge, 50+self.edge:getWidth()*2.15, 15+self.edge:getHeight()*1.65, math.rad(180), 1.15, 1.1, 0.5, 0.5)
+	love.graphics.draw(self.edge, 50, 15+self.edge:getHeight()*1.65, math.rad(270), 1.15, 1.1, 0.5, 0.5)
 
 	love.graphics.setShader(last_shader)
 end
