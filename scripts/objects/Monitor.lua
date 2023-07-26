abdc96fc46c2afb6536a1606e39c2e10ac8f1bf1
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-07-26 16:10:04 -0400

    Fix hover sound.

--- scripts/objects/Monitor.lua
+++ scripts/objects/Monitor.lua
@@ -56,7 +56,7 @@ function Monitor:init(mod_id, index)
 	
 	self.hovered = false
 	
-	self.ui_move = Assets.newSound("ui_move")
+	self.hover = Assets.newSound("wii/hover")
 	
 	self.font = Assets.getFont("main")
 	
@@ -88,7 +88,7 @@ function Monitor:update()
 	
 	if (mx / Kristal.getGameScale() > screen_x) and (mx / Kristal.getGameScale() < (screen_x + self.width)) and (my / Kristal.getGameScale() > screen_y) and (my / Kristal.getGameScale() < (screen_y + self.height)) then
 		if not self.hovered then
-			self.ui_move:play()
+			self.hover:play()
 			self.hovered = true
 			self:setLayer(1)
 		end
