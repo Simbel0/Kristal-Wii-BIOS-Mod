57234419b4d28e9c1d4c64017f59d8133e11167c
Liu Wenyuan <15816141883@163.com>
2024-02-19 12:53:45 +0800

    Rework Pregame, allow mod loading to go along with slideshow

--- scripts/globals/Pregame.lua
+++ scripts/globals/Pregame.lua
@@ -2,81 +2,175 @@
 local Pregame = {}
 
 function Pregame:init()
-	self.timer = LibTimer.new()
-	
-	self.stage = Stage()
-
 	Game.wii_menu = self
-	
-	self.substate = "MAIN"
+
+	self.slides = Utils.copy(Assets.getFrames("pregame/pregame"))
 end
 
-function Pregame:enter(_, maintenance)
-	self.time = 0
-	
-	self.first = false
-	
-	self.second = false
-	
-	self.alpha = 0
+Pregame.LOAD_STATUS = {
+	["NOT_STARTED"] = nil,
+	["WAITING"] = 0,
+	["LOADING"] = 1,
+	["LOADED"] = 2,
+}
 
-	self.screen_helper = ScreenHelper()
-	self.stage:addChild(self.screen_helper)
-	
-	self.first_sprite = Sprite("pregame/pregame_1")
-	self.first_sprite.alpha = 0
-	self.screen_helper:addChild(self.first_sprite)
-	
-	self.second_sprite = Sprite("pregame/pregame_2")
-	self.second_sprite.alpha = 0
-	self.screen_helper:addChild(self.second_sprite)
+function Pregame:enter(_, selection)
+	self.selected_mod = selection
+
+	self.wii_data = Utils.copy(Game.wii_data)
+
+	self.loading_mod = nil
+	self.going_to_miiware = select(1, Utils.startsWith(self.selected_mod, "wii_"))
+	if not self.going_to_miiware then
+		self.loading_mod = Pregame.LOAD_STATUS["WAITING"]
+	end
+
+	self.stage = Stage()
+
+	self.timer = Timer()
+	self.stage:addChild(self.timer)
+
+	self.slide = Sprite(self.slides[1], 0, 0)
+	self.slide.alpha = 0
+	self.slide.crossFadeTo = function(self, texture, time, fade_out, after)
+		self:crossFadeToSpeed(texture, (1 / (time or 1)) / 30 * (1 - self.crossfade_alpha), fade_out, after)
+	end
+	self.stage:addChild(self.slide)
+	self.slide_no = 1
+
+	self.mouse_prev = MOUSE_VISIBLE
+	Kristal.hideCursor()
+
+    self.state_manager = StateManager("", self, true)
+    self.state_manager:addState("INIT", {enter = self.startInit})
+    self.state_manager:addState("PRESENTING", {enter = self.startPresenting})
+    self.state_manager:addState("FADE", {enter = self.startFade})
+    self.state_manager:addState("EXIT", {update = self.enterGame})
+    self.state_manager:addState("DONE")
+	self.state_manager:setState("INIT")
+end
+
+function Pregame:clearModStatePhase1()
+	-- Stop music
+	Music.clear()
+
+	-- End the current mod
+	Kristal.callEvent("unload")
+	Mod = nil
+
+	Kristal.clearModHooks()
+	Kristal.clearModSubclasses()
+
+	-- Reset global variables
+	Registry.restoreOverridenGlobals()
+
+	package.loaded["src.engine.vars"] = nil
+	require("src.engine.vars")
+	-- Reset Game state
+	package.loaded["src.engine.game.game"] = nil
+	Kristal.States["Game"] = require("src.engine.game.game")
+	Game = Kristal.States["Game"]
+
+	Kristal.Mods.clear()
+
+	-- Restore assets and registry
+	Assets.restoreData()
+	Registry.initialize()
+end
+
+function Pregame:clearModStatePhase2()
+	-- Clear disruptive active globals
+	Object._clearCache()
+	Draw._clearStacks()
+
+	love.window.setIcon(Kristal.icon)
+	love.window.setTitle(Kristal.getDesiredWindowTitle())
+end
+
+function Pregame:startInit()
+	self.slide:fadeTo(1, 1, function() self:setState("PRESENTING") end)
+end
+
+function Pregame:startPresenting()
+	self.timer:after(5, function() self:setState("FADE") end)
+end
+
+function Pregame:startFade()
+	self.slide_no = self.slide_no + 1
+	if self.slide_no <= #self.slides then
+		self.slide:crossFadeTo(self.slides[self.slide_no], 1, false, function()
+			self:setState("PRESENTING")
+		end)
+	else
+		self.slide:fadeTo(0, 1, function() self:setState("EXIT") end)
+	end
+end
+
+function Pregame:enterGame()
+	if self.mouse_prev then Kristal.showCursor() end
+	if self.going_to_miiware then
+		if self.selected_mod == "wii_rtk" then -- All of this is temporary
+			Kristal.load_wii_mod = false
+			Kristal.load_wii = false
+			Kristal.returnToMenu()
+		elseif self.selected_mod == "wii_food" then
+			love.system.openURL("https://www.dominos.com/en/")
+			Mod:setState("MainMenu", false)
+		elseif self.selected_mod == "wii_mii" then
+			Mod:setState("MiiChannel", false)
+		end
+	elseif self.loading_mod ~= 2 then
+		return
+	else
+		self:clearModStatePhase2()
+		local savemenu_vanilla = SaveMenu
+		if Kristal.preInitMod(self.selected_mod) then
+			if SaveMenu ~= savemenu_vanilla then
+				print("WARNING: SaveMenu is not vanilla")
+			end
+			if WiiSaveMenu then
+				Registry.registerGlobal("SaveMenu", WiiSaveMenu, true)
+			else
+				Registry.registerGlobal("SaveMenu", SimpleSaveMenu, true)
+			end
+			Gamestate.switch(Kristal.States["Game"], 0, name)
+		end
+	end
+	self:setState("DONE")
 end
 
 function Pregame:update()
-	self.time = self.time + DT
-	if not self.first then
-		if self.time < 1 then
-			self.first_sprite.alpha = self.time
-		else
-			self.first_sprite.alpha = 1
-			self.first = true
-		end
-	end
-	if not self.second then
-		if self.time < 6 then
-			self.second_sprite.alpha = self.time - 5
-		else
-			self.second_sprite.alpha = 1
-			self.first_sprite.alpha = 0
-			self.second = true
-		end
-	else
-		if self.time < 11 then
-			self.second_sprite.alpha = 1 - (self.time - 10)
-		else
-			self.second_sprite.alpha = 0
-			if Game:getFlag("selected_mod") == "wii_rtk" then -- All of this is temporary
-				Kristal.load_wii_mod = false
-				Kristal.load_wii = false
-				Kristal.returnToMenu()
-			elseif Game:getFlag("selected_mod") == "wii_food" then
-				love.system.openURL("https://www.dominos.com/en/")
-				Mod:setState("MainMenu", false)
-			elseif Game:getFlag("selected_mod") == "wii_mii" then
-				Mod:setState("MiiChannel", false)
-			else
-				Mod:loadMod(Game:getFlag("selected_mod"))
-			end
-		end
-	end
-	
-	self.screen_helper:update()
+	if self.loading_mod == self.LOAD_STATUS["WAITING"] then
+		assert(Kristal.Mods.data[self.selected_mod], "No mod \""..tostring(self.selected_mod).."\"")
+		self.loading_mod = self.LOAD_STATUS["LOADING"]
+		self:clearModStatePhase1()
+		Kristal.load_wii_mod = true
 
+		Kristal.loadAssets("","mods","", function()
+			Kristal.loadMod(self.selected_mod, 0, self.wii_data["name"], function()
+				self.loading_mod = self.LOAD_STATUS["LOADED"]
+			end)
+		end)
+	end
+
+	self.state_manager:update()
 	self.stage:update()
 end
 
+function Pregame:setState(...) self.state_manager:setState(...) end
+
 function Pregame:draw()
-    self.screen_helper:draw()
+    love.graphics.clear(0, 0, 0, 1)
+    love.graphics.push()
+    if Kristal.callEvent("preDraw") then
+        love.graphics.pop()
+        return
+    end
+    love.graphics.pop()
+    self.stage:draw()
+    love.graphics.push()
+    Kristal.callEvent("postDraw")
+    love.graphics.pop()
 end
 
 return Pregame
