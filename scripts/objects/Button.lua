ec36b6e944d31ddf31379505c6e688932200d59a
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2025-03-30 13:51:28 -0400

    Mouse button pressed

--- scripts/objects/Button.lua
+++ scripts/objects/Button.lua
@@ -38,7 +38,7 @@ function Button:update()
 					self.played_sound = true
 					Assets.playSound("wii/hover")
 				end
-				if not self.pressed and love.mouse.isDown(1) then
+				if not self.pressed and Input.mousePressed(1) then
 					self.pressed = true
 					self:onClick()
 				end
